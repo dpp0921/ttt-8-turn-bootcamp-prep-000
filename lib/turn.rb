256efaea4a83e53997f71a4323e640fe3b6f7070
Chen Ye <dpp0921@gmail.com>
2018-01-18 21:11:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,12 +12,8 @@ def display_board(board)
 end
 
 def valid_move(board, index)
-  if index.between?(1, 9)
-    if !position_taken(board, index)
-      true
-    end
-  end
-  index.between?(0, 8) && !position_taken(board, index)
+  if index.between?(0, 8) && !position_taken(board, index)
+    
 end
 
 def position_taken(board, index)
